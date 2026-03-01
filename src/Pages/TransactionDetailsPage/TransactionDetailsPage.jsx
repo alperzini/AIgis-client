568de6c8816737e9cce3781e40157b6373960085
Kavin Paul <kavinp622@gmail.com>
2026-03-01 06:57:42 -0700

    Initial Setup for Transactions Details Page

--- src/Pages/TransactionDetailsPage/TransactionDetailsPage.jsx
+++ src/Pages/TransactionDetailsPage/TransactionDetailsPage.jsx
@@ -1,19 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import PageWrapper from "../../components/PageWrapper/PageWrapper";
 import "./TransactionDetailsPage.scss";
 import NotesThread from "../../components/NotesThread/NotesThread";
 import OrderDetailsCard from "../../components/OrderDetailsCard/OrderDetailsCard";
 import AccountSummaryCard from "../../components/AccountSummaryCard/AccountSummaryCard";
+import Typography from "../../components/Typography/Typography";
+import ArrowBack from "../../assets/icons/arrow_back-24px.svg";
 
 function TransactionDetailsPage() {
   const { transactionId } = useParams();
+  const navigate = useNavigate();
 
   return (
     <main className="transaction-details-page">
+      <div className="transaction-details-page__header">
+        <button className="transaction-details-page__back-button" onClick={() => navigate("/transactions")}>
+          <img src={ArrowBack} alt="back-arrow" />
+        </button>
+        <div className="transaction-details-page__header-text-wrapper">
+          <Typography className="transaction-details-page__header-text">Transaction ID: {transactionId}</Typography>
+          <Typography className="transaction-details-page__header-text">Active Editor: #38-101-031</Typography>
+        </div>
+      </div>
+
       <p className="transaction-details-page__text">
         Transaction ID: {transactionId}
       </p>
-      {/* Testing */}
       <AccountSummaryCard />
       <OrderDetailsCard />
       <NotesThread />
